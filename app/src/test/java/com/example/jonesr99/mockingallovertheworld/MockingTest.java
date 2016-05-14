3dec5702d0680cfc5d24b9c0dfc16cbe90129f37
Rachel Jones <Rachel.Jones02@bbc.co.uk>
2016-05-14 15:06:38 +0100

    Set return type of a mocked object (only tested for int)

--- app/src/test/java/com/example/jonesr99/mockingallovertheworld/MockingTest.java
+++ app/src/test/java/com/example/jonesr99/mockingallovertheworld/MockingTest.java
@@ -6,6 +6,8 @@ import java.lang.reflect.InvocationHandler;
 import java.lang.reflect.Method;
 import java.lang.reflect.Proxy;
 import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.HashMap;
 
 import static org.junit.Assert.*;
 
@@ -20,7 +22,7 @@ public class MockingTest {
     }
 
     @Test
-    public void givenMyInterfaceHasAMethodICanVerifyItsInvocation() throws Exception {
+    public void givenMyMethodIsCalledThenICanVerify() throws Exception {
         MockedWrapper<MethodTestInterface> mockObject = Mocker.mock(MethodTestInterface.class);
         mockObject.get().one();
         mockObject.verifyCalled().one();
@@ -32,12 +34,37 @@ public class MockingTest {
         mockedObject.verifyCalled().one();
     }
 
+    @Test
+    public void givenMyMethodIsCalledWithParamsThenICanVerify() throws Exception {
+        MockedWrapper<MethodTestInterface> mockObject = Mocker.mock(MethodTestInterface.class);
+        mockObject.get().add(1, 2);
+        mockObject.verifyCalled().add(1, 2);
+    }
+
+    @Test (expected = AssertionError.class)
+    public void givenMyMethodIsCalledWithIncorrectParamsAssertionErrorIsThrown() throws Exception {
+        MockedWrapper<MethodTestInterface> mockObject = Mocker.mock(MethodTestInterface.class);
+        mockObject.get().add(1, 1);
+        mockObject.verifyCalled().add(1, 2);
+    }
+
+    @Test
+    public void givenISpecifyReturnValueThenThatIsReturned() throws Exception {
+        MockedWrapper<MethodTestInterface> mockObject = Mocker.mock(MethodTestInterface.class);
+        assertNotNull(mockObject.get());
+        MethodTestInterface proxy = mockObject.returns(7);
+        proxy.seven();
+        assertEquals(mockObject.get().seven(), 7);
+    }
+
     public interface TestInterface {
 
     }
 
     public interface MethodTestInterface {
         void one();
+        void add(int numberOne, int numberTwo);
+        int seven();
     }
 
     private static class Mocker {
@@ -49,18 +76,55 @@ public class MockingTest {
 
     }
 
+    private static class MethodCalledDetails {
+        public final Method method;
+        public final Object[] args;
+
+        private MethodCalledDetails(Method method, Object[] args) {
+            this.args = args;
+            this.method = method;
+        }
+
+        @Override
+        public boolean equals(Object o) {
+            if (this == o) return true;
+            if (o == null || getClass() != o.getClass()) return false;
+
+            MethodCalledDetails that = (MethodCalledDetails) o;
+
+            if (method != null ? !method.equals(that.method) : that.method != null) return false;
+            // Probably incorrect - comparing Object[] arrays with Arrays.equals
+            return Arrays.equals(args, that.args);
+
+        }
+
+        @Override
+        public int hashCode() {
+            int result = method != null ? method.hashCode() : 0;
+            result = 31 * result + Arrays.hashCode(args);
+            return result;
+        }
+    }
+
     private static class MockInvocationHandler implements InvocationHandler {
 
-        private ArrayList<Method> called = new ArrayList<>();
+        private ArrayList<MethodCalledDetails> called = new ArrayList<>();
+        private HashMap<MethodCalledDetails, Object> returnValues = new HashMap<>();
 
         @Override
         public Object invoke(Object proxy, Method method, Object[] args) throws Throwable {
-            called.add(method);
-            return null;
+            MethodCalledDetails methodCalledDetails = new MethodCalledDetails(method, args);
+            called.add(methodCalledDetails);
+
+            return returnValues.get(methodCalledDetails);
         }
 
-        public boolean hasBeenCalled(Method method) {
-            return called.contains(method);
+        public boolean hasBeenCalled(Method method, Object[] args) {
+            return called.contains(new MethodCalledDetails(method, args));
+        }
+
+        public void setValueForMethod(Method method, Object[] args, Object retVal) {
+            returnValues.put(new MethodCalledDetails(method, args), retVal);
         }
     }
 
@@ -82,7 +146,7 @@ public class MockingTest {
             return createProxy(new InvocationHandler() {
                 @Override
                 public Object invoke(Object proxy, Method method, Object[] args) throws Throwable {
-                    assertTrue(MockedWrapper.this.invocationHandler.hasBeenCalled(method));
+                    assertTrue(invocationHandler.hasBeenCalled(method, args));
                     return null;
                 }
             });
@@ -90,6 +154,38 @@ public class MockingTest {
 
         private T createProxy(InvocationHandler invocationHandler) {
             return (T) Proxy.newProxyInstance(getClass().getClassLoader(), mockedObject.getClass().getInterfaces(), invocationHandler);
+        }
+
+        public T returns(final Object retVal) {
+            return createProxy(new InvocationHandler() {
+                @Override
+                public Object invoke(Object proxy, Method method, Object[] args) throws Throwable {
+                    invocationHandler.setValueForMethod(method, args, retVal);
+                    return safeReturnNull(method.getReturnType());
+                }
+            });
+        }
+
+        private Object safeReturnNull(Class<?> returnType) {
+            //boolean , byte , char , short , int , long , float and double
+            if(returnType.equals(Boolean.class)) {
+                return false;
+            } else if (returnType.equals(byte.class)) {
+                return (byte) 0;
+            } else if (returnType.equals(char.class)) {
+                return '\000';
+            } else if (returnType.equals(short.class)) {
+                return (short) 0;
+            } else if (returnType.equals(int.class)) {
+                return 0;
+            } else if (returnType.equals(long.class)) {
+                return 0L;
+            } else if (returnType.equals(float.class)) {
+                return  0f;
+            } else if (returnType.equals(double.class)) {
+                return  0d;
+            }
+            else return null;
         }
     }
 }
