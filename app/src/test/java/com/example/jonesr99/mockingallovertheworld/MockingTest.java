f6fe6c84746b3c1799baba3d021019bb0dc4dd7d
James Shephard <james.shephard@bbc.co.uk>
2016-05-14 12:15:03 +0100

    added tests for method invocation

--- app/src/test/java/com/example/jonesr99/mockingallovertheworld/MockingTest.java
+++ app/src/test/java/com/example/jonesr99/mockingallovertheworld/MockingTest.java
@@ -4,6 +4,8 @@ import org.junit.Test;
 
 import java.lang.reflect.InvocationHandler;
 import java.lang.reflect.Method;
+import java.lang.reflect.Proxy;
+import java.util.ArrayList;
 
 import static org.junit.Assert.*;
 
@@ -13,44 +15,63 @@ import static org.junit.Assert.*;
 public class MockingTest {
     @Test
     public void givenIHaveAnInterfaceThenICanInstantiateAMock() throws Exception {
-        Mocked<TestInterface> mockObject = Mocking.mock(TestInterface.class);
+        MockedWrapper<TestInterface> mockObject = Mocker.mock(TestInterface.class);
         assertNotNull(mockObject);
     }
 
     @Test
     public void givenMyInterfaceHasAMethodICanVerifyItsInvocation() throws Exception {
-        Mocked<MethodTestInterface> mockObject = Mocking.mock(MethodTestInterface.class);
-
+        MockedWrapper<MethodTestInterface> mockObject = Mocker.mock(MethodTestInterface.class);
         mockObject.get().one();
         mockObject.verifyCalled().one();
     }
 
+    @Test (expected = AssertionError.class)
+    public void givenMyMethodIsNotCalledAssertionErrorIsThrown() throws Exception {
+        MockedWrapper<MethodTestInterface> mockedObject = Mocker.mock(MethodTestInterface.class);
+        mockedObject.verifyCalled().one();
+    }
 
     public interface TestInterface {
 
     }
 
     public interface MethodTestInterface {
-        public void one();
+        void one();
     }
 
-    private static class Mocking {
-        public static <T> Mocked<T> mock(Class<T> clazz) throws IllegalAccessException, InstantiationException {
-            return new Mocked(java.lang.reflect.Proxy.newProxyInstance(clazz.getClassLoader(), new Class<?>[]{clazz}, new InvocationHandler() {
-                @Override
-                public Object invoke(Object proxy, Method method, Object[] args) throws Throwable {
-                    return null;
-                }
-            }));
+    private static class Mocker {
+        public static <T> MockedWrapper<T> mock(Class<T> clazz) throws IllegalAccessException, InstantiationException {
+            MockInvocationHandler invocationHandler = new MockInvocationHandler();
+            Object proxyInstance = Proxy.newProxyInstance(clazz.getClassLoader(), new Class<?>[]{clazz}, invocationHandler);
+            return new MockedWrapper(proxyInstance, invocationHandler);
+        }
+
+    }
+
+    private static class MockInvocationHandler implements InvocationHandler {
+
+        private ArrayList<Method> called = new ArrayList<>();
+
+        @Override
+        public Object invoke(Object proxy, Method method, Object[] args) throws Throwable {
+            called.add(method);
+            return null;
+        }
+
+        public boolean hasBeenCalled(Method method) {
+            return called.contains(method);
         }
     }
 
+    private static class MockedWrapper<T> {
 
-    private static class Mocked<T> {
-        private T mockedObject;
+        private final T mockedObject;
+        private final MockInvocationHandler invocationHandler;
 
-        public Mocked(T mockedObject) {
+        public MockedWrapper(T mockedObject, MockInvocationHandler invocationHandler) {
             this.mockedObject = mockedObject;
+            this.invocationHandler = invocationHandler;
         }
 
         public T get() {
@@ -58,7 +79,17 @@ public class MockingTest {
         }
 
         public T verifyCalled() {
-            return null;
+            return createProxy(new InvocationHandler() {
+                @Override
+                public Object invoke(Object proxy, Method method, Object[] args) throws Throwable {
+                    assertTrue(MockedWrapper.this.invocationHandler.hasBeenCalled(method));
+                    return null;
+                }
+            });
+        }
+
+        private T createProxy(InvocationHandler invocationHandler) {
+            return (T) Proxy.newProxyInstance(getClass().getClassLoader(), mockedObject.getClass().getInterfaces(), invocationHandler);
         }
     }
 }
